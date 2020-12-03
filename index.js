dc4cc1daf1a038f17ed6b8c242642cd9523d0a33
Francis Rubio <67542653+maniczirconium@users.noreply.github.com>
2020-12-03 19:46:27 +0800

    Removed trailing slash on CORS origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
 const server = app.listen(process.env.PORT || 1337, () => console.log('Webhook is listening: ' + process.env.PORT || 1337))
 const io = socket(server, {
   cors: {
-    origin: "https://localhost:8000/",
+    origin: "https://localhost:8000",
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true
