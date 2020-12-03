f3a01a8889c2625b21cbf10b9435cd9edabdafa7
Francis Rubio <67542653+maniczirconium@users.noreply.github.com>
2020-12-03 16:48:07 +0800

    Added support for automated responses

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express    = require('express'),
       bodyParser = require('body-parser'),
+      request    = require('request')
       app        = express().use(bodyParser.json());
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
@@ -18,6 +19,12 @@ app.post('/webhook', (req, res) => {
 
       const sender_psid = webhook_event.sender.id
       console.log(`Sender PSID: ${sender_psid}`)
+    
+      if(webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message)
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback)
+      }
     })
 
     res.status(200).send('EVENT_RECEIVED')
@@ -42,8 +49,40 @@ app.get('/webhook', (req, res) => {
   }
 })
 
-function handleMessage(sender_psid, received_message) {}
+function handleMessage(sender_psid, received_message) {
+  let response
+
+  if(received_message.text) {
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }
+
+  callSendAPI(sender_psid, response)
+}
 
 function handlePostback(sender_psid, received_postback) {}
 
-function callSendAPI(sender_psid, response) {}
+function callSendAPI(sender_psid, response) {
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs" : {
+      "access_token": process.env.PAGE_ACCESS_TOKEN
+    },
+    "method": "POST",
+    "json"  : request_body
+  }, (err, res, body) {
+    if(!err) {
+      console.log("Message sent!")
+    } else {
+      console.error("Unable to send message: " + err)
+    }
+  })
+}
